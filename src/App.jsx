23a3dc1f984822f03af92febf5ad355a30b9d185
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-29 20:44:27 +0530

    We are here

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
-import { useState } from "react";
-import data from "./data";
-import Accordion from "./Accordion";
 const App = () => {
-  const [questions, setQuestions] = useState(data);
-
-  const handleToggle = (id) => {
-    console.log(id);
-    const updatedQuestions = questions.map((question) => {
-      if (question.id === id) {
-        return { ...question, isOpen: !question.isOpen };
-      }
-      return question;
-    });
-
-    setQuestions(updatedQuestions);
-  };
-
   return (
     <main>
-      <div className="container">
-        <h1>Questions</h1>
-
-        {questions.map((question) => {
-          return (
-            <Accordion
-              {...question}
-              id={question.id}
-              handleToggle={handleToggle}
-            />
-          );
-        })}
-      </div>
+      <h1>Accordion React-18</h1>
     </main>
   );
 };
