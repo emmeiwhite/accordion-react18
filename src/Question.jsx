941e5595c1326a5f62ae354dc8e35a5280380749
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-11 01:26:29 +0530

    Accordion Done! Fully

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 
-export default function Question({ title, info }) {
-  const [isOpen, setIsOpen] = useState(false)
+export default function Question({ id, title, info, currentIndex, handleClick }) {
+  //   const [isOpen, setIsOpen] = useState(false)
+  const isOpen = id === currentIndex
   return (
     <article className="question">
       <header>
         <h5>{title}</h5>
         <button
           className="question-btn"
-          onClick={() => setIsOpen(!isOpen)}
+          onClick={() => handleClick(id)}
         >
           {isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
--- src/QuestionsList.jsx
+++ src/QuestionsList.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react'
 import Question from './Question'
 export default function QuestionsList({ questions }) {
+  const [currentSelectedIndex, setCurrentSelectedIndex] = useState(-1)
+
+  const handleClick = id => {
+    console.log(id)
+    if (id !== currentSelectedIndex) {
+      setCurrentSelectedIndex(id)
+    } else {
+      setCurrentSelectedIndex(-1)
+    }
+  }
   return (
     <section className="container">
-      {questions.map(question => {
+      {questions.map((question, index) => {
         return (
           <Question
             {...question}
-            id={question.id}
+            key={question.id}
+            currentIndex={currentSelectedIndex}
+            handleClick={handleClick}
           />
         )
       })}
